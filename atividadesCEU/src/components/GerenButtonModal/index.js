48173c447c7e831e5ab0df630350243bfc18fbd1
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2021-03-30 03:05:03 -0300

    atual

--- atividadesCEU/src/components/GerenButtonModal/index.js
+++ atividadesCEU/src/components/GerenButtonModal/index.js
@@ -18,11 +18,70 @@ const GerenButtonModal = ({isVisible, onCancel}) => {
         
         onBackdropPress={onCancel}>
             <View>
-                <TouchableOpacity onPress={
+            <View style={styles.menulinha1}>
+                    
+                    <TouchableOpacity 
+                    style={styles.botão}
+                    onPress={
                     onCancel
-                }>
-                <Text>Usuários</Text>
-                </TouchableOpacity>
+                    }>
+                    </TouchableOpacity>
+                    
+
+                    <View >
+                    <TouchableOpacity 
+                    style={styles.botão}
+                    onPress={
+                    onCancel
+                    }>
+                     <Text style={styles.botão2Text}>U</Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
+
+
+                <View style={styles.menulinha1}>
+                    
+                    <TouchableOpacity 
+                    style={styles.botão1}
+                    onPress={
+                    onCancel
+                    }>
+                    </TouchableOpacity>
+                    
+
+                    <View>
+                    <TouchableOpacity onPress={
+                    onCancel
+                    }>
+                     <Text style={styles.botão2Text}>Usuários</Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
+
+
+                <View style={styles.menulinha1}>
+                    
+                    <TouchableOpacity 
+                    style={styles.botão1}
+                    onPress={
+                    onCancel
+                    }>
+                    </TouchableOpacity>
+                    
+
+                    <View>
+                    <TouchableOpacity onPress={
+                    onCancel
+                    }>
+                     <Text style={styles.sair}>Sair</Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
+
+
+
+                
 
                 <TouchableOpacity
                 style={styles.modal}
@@ -39,11 +98,49 @@ const GerenButtonModal = ({isVisible, onCancel}) => {
 }
 
 const styles = StyleSheet.create({
+    menulinha1: {
+        //flex: 1,
+        flexDirection: 'row',
+    },
+    botão: {
+        //marginTop: 90,
+        paddingTop: 90,
+        flex: 2,
+        //backgroundColor: Colors.black,
+        justifyContent: 'center',
+    },
+    botão1: {
+        //marginTop: 90,
+        flex: 2,
+        //backgroundColor: Colors.black,
+        justifyContent: 'center',
+    },
+    botão2: {
+        flex: 1,
+        //marginTop: 90,
+        backgroundColor: Colors.green,
+        justifyContent: 'space-around',
+    },
+    botão3: {
+        flex: 1,
+        //marginTop: 90,
+        backgroundColor: Colors.champagne,
+        justifyContent: 'center',
+    },
+    botão2Text: {
+        justifyContent: 'flex-end',
+        fontSize: 18,
+        color: Colors.black,
+    },
     modal: {
         flex: 1,
-        //backgroundColor: Colors.background,
+       // backgroundColor: Colors.background,
         marginVertical: 5,
         padding: 500,
+    },
+    sair: {
+        color: Colors.red,
+        fontSize: 17,
     }
 });
 export default GerenButtonModal
