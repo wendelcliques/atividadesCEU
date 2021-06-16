e184a7358e56df6d50bd388214dbc9df1d47b6d7
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2021-06-16 02:20:52 -0300

    atual

---
+++ atividadesCEU/src/pages/UsuarioEntry/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const UsuarioEntry = () => {
+    return (
+        <View>
+            <Text>usuário entry</Text>
+        </View>
+    )
+}
+
+export default UsuarioEntry
