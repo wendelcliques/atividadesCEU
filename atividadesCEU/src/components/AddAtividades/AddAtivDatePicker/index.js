460405ce9e021348ae54b0c75fcabf2d63568a94
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2021-07-07 23:51:49 -0300

    atual

--- atividadesCEU/src/components/AddAtividades/AddAtivDatePicker/index.js
+++ atividadesCEU/src/components/AddAtividades/AddAtivDatePicker/index.js
@@ -1,13 +1,19 @@
 /* eslint-disable prettier/prettier */
 import React, {useState} from 'react'
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
+import {Modal, View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
+import moment from '../../../DateTimePtbr/moment';
 
 
 
 const AddAtivDatePicker = ({value, onChange}) => {
     const [modalVisible, setModalVisible] = useState(false);
+
+    const [modalVisibleSemanal, setModalVisibleSemanal] = useState(false);
+
+    const [modalVisivel, setModalVisivel] = useState(false);
 
     const onChangeValue = date => {
         onChange(date);
@@ -16,14 +22,35 @@ const AddAtivDatePicker = ({value, onChange}) => {
 
     const onCancel = () => {
         setModalVisible(false);
+        setModalVisibleSemanal(false);
+        setModalVisivel(false);
+    };
+
+    const onVisible = () => {
+        
+        setModalVisivel(true)
     }
+
+    const onTimeVisible = () => {
+        setModalVisibleSemanal(true);
+       
+    }
+
     return (
         <View>
             <TouchableOpacity
           onPress={() => setModalVisible(true)}
             >
-            <Text>Agenda</Text>
-            <Text>{value.toString()}</Text>
+            <Text>Agenda Mensal</Text>
+            <Text>{moment(value.toDate()).calendar()}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+          onPress={() => onVisible()
+        }
+            >
+            <Text>Agenda Semanal</Text>
+            <Text>{moment(value.toDate()).calendar()}</Text>
             </TouchableOpacity>
 
             <DateTimePickerModal
@@ -35,6 +62,45 @@ const AddAtivDatePicker = ({value, onChange}) => {
             onCancel={onCancel}
             
             />
+            <Modal
+           animationType='slide'
+           transparent={false}
+           visible={modalVisivel} 
+            
+            >
+
+                <Text>teste modal</Text>
+
+
+                <TouchableOpacity
+          onPress={() => onTimeVisible()
+        }
+            >
+            <Text>OK</Text>
+            
+            </TouchableOpacity>
+
+                <TouchableOpacity
+          onPress={() => onCancel()
+        }
+            >
+            <Text>CANCELAR</Text>
+            
+            </TouchableOpacity>
+
+
+<DateTimePickerModal
+            isVisible={modalVisibleSemanal}
+            mode="time"
+            date={value}
+            
+            onConfirm={onChangeValue}
+            onCancel={onCancel}
+            
+            
+            />
+
+</Modal>
 
         </View>
     )
